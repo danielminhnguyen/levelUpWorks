4b1993d32d5371e4a3076a5103c55158e7b6d414
DANIEL NGUYEN <hokkinxx@DANIELs-MBP.lan>
2021-03-04 13:38:16 +1300

    organise folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 import DashBoard from "layouts/Dashboard";
 import Main from "layouts/Main";
+import Student from "mission6/Student";
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Profile from "screens/Profile";
 
 export default function App() {
   return (
     <div>
       <Switch>
-        <Route path="/dashboard" component={DashBoard} />
+        {/* <Route path="/dashboard" component={DashBoard} />
         <Route path="/projects/:projectid" component={DashBoard} />
-        <Route path="/" component={Main} />
+        <Route path="/" component={Main} /> */}
+
+        <Route exact path="/student" component={Student} />
+        <Route exact path="/profile" component={Profile} />
       </Switch>
     </div>
   );
